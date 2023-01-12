f1076c9be360cab8795a696ce33bd49b4250862e
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-12 13:38:18 +0000

    News Item Added

--- my-app/src/components/News.js
+++ my-app/src/components/News.js
@@ -9,9 +9,6 @@ export class news extends Component {
         <NewsItem/>
         <NewsItem/>
         <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
         </div>
     )
   }
