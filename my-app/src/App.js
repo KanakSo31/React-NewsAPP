89710ee63c7d291b3ee7b87f5045f8aee8cb6e01
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-23 09:12:17 +0000

    error under process

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link
 } from "react-router-dom";
 
 export default class App extends Component {
@@ -15,15 +16,15 @@ export default class App extends Component {
       <div className='container-flex'>
         kanak
         <Router>
-          <Navbar/>
+        <Navbar/>
           <Routes>
             <Route path="/"><News pageSize = {6} country= "in" category = "general"/></Route>
-            <Route path="/business"><News pageSize = {6} country= "in" category = "business"/></Route>
+            {/* <Route path="/business"><News pageSize = {6} country= "in" category = "business"/></Route>
             <Route path="/entertainment"><News pageSize = {6} country= "in" category = "entertainment"/></Route>
             <Route path="/health"><News pageSize = {6} country= "in" category = "health"/></Route>
             <Route path="/science"><News pageSize = {6} country= "in" category = "science"/></Route>
             <Route path="/sport"><News pageSize = {6} country= "in" category = "sport"/></Route>
-            <Route path="/technology"><News pageSize = {6} country= "in" category = "technology"/></Route>
+            <Route path="/technology"><News pageSize = {6} country= "in" category = "technology"/></Route> */}
           </Routes>
         </Router>
       </div>
