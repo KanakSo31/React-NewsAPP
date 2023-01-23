06e44b72d672b1d9151d67aadbf1923b8624792d
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-23 09:53:16 +0000

    error checking

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,7 @@ import News from './components/News';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 export default class App extends Component {
@@ -14,17 +13,16 @@ export default class App extends Component {
   render() {
     return (
       <div className='container-flex'>
-        kanak
         <Router>
         <Navbar/>
           <Routes>
             <Route path="/"><News pageSize = {6} country= "in" category = "general"/></Route>
-            {/* <Route path="/business"><News pageSize = {6} country= "in" category = "business"/></Route>
+            <Route path="/business"><News pageSize = {6} country= "in" category = "business"/></Route>
             <Route path="/entertainment"><News pageSize = {6} country= "in" category = "entertainment"/></Route>
             <Route path="/health"><News pageSize = {6} country= "in" category = "health"/></Route>
             <Route path="/science"><News pageSize = {6} country= "in" category = "science"/></Route>
             <Route path="/sport"><News pageSize = {6} country= "in" category = "sport"/></Route>
-            <Route path="/technology"><News pageSize = {6} country= "in" category = "technology"/></Route> */}
+            <Route path="/technology"><News pageSize = {6} country= "in" category = "technology"/></Route>
           </Routes>
         </Router>
       </div>
