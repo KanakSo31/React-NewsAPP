84582cd90c6d3b47b998a4b84066f6d669b4035a
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-01-30 07:34:55 +0000

    initial changes

--- my-app/src/components/News.js
+++ my-app/src/components/News.js
@@ -26,16 +26,16 @@ export class News extends Component {
   }
   // componentdidMount fetch all the information with the help of API
   async componentDidMount() {
-    console.log(this.props);
+    // console.log(this.props);
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8c9b938866be46d694e319f6d069d1e0&page=1&pageSize=${this.props.pageSize}`;
-console.log(url);
+// console.log(url);
     this.setState({ loading: true });
 
     let data = await fetch(url);
 
     let parsedData = await data.json()
 
-    console.log(parsedData);
+    // console.log(parsedData);
 
     this.setState({
       articles: parsedData.articles,
@@ -55,7 +55,7 @@ console.log(url);
 
     let parsedData = await data.json()
 
-    console.log(parsedData);
+    // console.log(parsedData);
 
     this.setState({
 
@@ -69,18 +69,18 @@ console.log(url);
   handleNextClick = async () => {
 
     console.log("Next");
-    console.log(this.props);
+    // console.log(this.props);
 
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
 
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8c9b938866be46d694e319f6d069d1e0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      console.log(url);
+      // console.log(url);
       this.setState({ loading: true });
 
       let data = await fetch(url);
 
       let parsedData = await data.json();
-      console.log(parsedData.articles);
+      // console.log(parsedData.articles);
       this.setState({
 
         page: this.state.page + 1,
