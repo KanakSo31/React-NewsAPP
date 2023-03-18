1fcb48239c7c1842b7d9e0974d0d68febd5f0c67
kanak sonowal <94075309+KanakSo31@users.noreply.github.com>
2023-03-18 13:12:24 +0000

    adding badge

--- my-app/src/components/NewsItem.js
+++ my-app/src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "equal" }}>
+        <span class="position-absolute top-2 translate-middle badge rounded-pill bg-warning" style={{left:'85%',zindex:'1'}}>{source}
+            </span>
           <img src={!imgUrl ? "https://images.hindustantimes.com/tech/img/2023/01/20/1600x900/30dor_0_1674186986073_1674186993798_1674186993798.jpg" : imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
-              {source}
-            </span></h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <p className='card-text'><small className='text-muted'>By{!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
             <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
